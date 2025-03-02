049b52c89d9210015065582818493acea93f9405
shantanu-raje <shantanuraje17@gmail.com>
2025-03-02 11:14:13 +0530

    Updated app.py and passwd.env to fix connection strings

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import pyodbc
 from azure.storage.blob import BlobServiceClient
 import os 
 from dotenv import load_dotenv
+
 load_dotenv()
+
 app = Flask(__name__)
 
-# Azure SQL Connection String
+# Environment Variables
 conn_str = os.getenv("CONN_STR")
+blob_service_client = BlobServiceClient.from_connection_string(os.getenv("AZURE_BLOB_STORAGE_CONN_STR"))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -18,7 +21,7 @@ def index():
         name = request.form["name"]
         price = float(request.form["price"])
         description = request.form["description"]
-        image_url = request.form["image_url"]  # Later we'll use blob storage here
+        image_url = request.form["image_url"]
 
         cursor.execute("INSERT INTO Products (ProductName, Price, Description, ImageURL) VALUES (?, ?, ?, ?)",
                        (name, price, description, image_url))
@@ -30,15 +33,14 @@ def index():
     conn.close()
     return render_template("index.html", products=products)
 
-blob_service_client = BlobServiceClient.from_connection_string(os.getenv("AZURE_BLOB_STORAGE_CONN_STR"))
-
 @app.route("/upload", methods=["POST"])
 def upload_image():
     file = request.files['image']
     blob_client = blob_service_client.get_blob_client(container="product-images", blob=file.filename)
-    blob_client.upload_blob(file)
+    blob_client.upload_blob(file, overwrite=True)
 
-    image_url = f"https://{productcataloguestorage}.blob.core.windows.net/products/{file.filename}"
+    account_name = "productcataloguestorage"
+    image_url = f"https://{account_name}.blob.core.windows.net/product-images/{file.filename}"
     return image_url
 
 if __name__ == "__main__":
